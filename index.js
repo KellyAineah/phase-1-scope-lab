eb0fa4c5ebdb9dcfaaa93befc5f91d31a713c6b1
KellyAineah <kellyainea99@gmail.com>
2024-03-21 02:54:09 +0300

    adds scope

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob";
+    
+} 
+const leastFavoriteCustomer = "jon";
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = "kelly";
+}
